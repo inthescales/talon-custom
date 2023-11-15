24d4161345cf69eed3b8d83d3c7c3054bdf983fc
Robin Hill <rehill012@gmail.com>
2023-11-15 15:51:12 -0500

    Add sublime text commands

---
+++ apps/sublime text/sublime_text.py
@@ -0,0 +1,40 @@
+from talon import Context, actions
+
+# Context matching
+ctx = Context()
+ctx.matches = r"""
+os: mac
+app: sublime_text
+"""
+
+@ctx.action_class("app")
+class AppActions:
+    def preferences():
+        actions.key("cmd-,")
+
+    def tab_next():
+        actions.key("cmd-shift-]")
+
+    def tab_previous():
+        actions.key("cmd-shift-[")
+
+    def tab_open():
+        actions.key("cmd-n")
+
+    def tab_reopen():
+        actions.key("cmd-shift-t")
+
+    def window_open():
+        return
+
+    def window_close():
+        actions.key("cmd-shift-w")
+
+@ctx.action_class("user")
+class UserActions:
+    def tab_jump(number: int):
+        if number <= 9:
+            actions.key(f"cmd-{number}")
+
+    def tab_final():
+        actions.key("cmd-9")
---
+++ apps/sublime text/sublime_text.talon
@@ -0,0 +1,6 @@
+app: sublime_text
+-
+tag(): user.find_and_replace
+tag(): user.line_commands
+tag(): user.splits
+tag(): user.tabs
